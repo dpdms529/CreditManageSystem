3e37911ebdc0b6e83eac1a276ffc7d51a400705f
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-03-20 09:04:54 +0900

    Feat : crawling
    1. 크롤링 코드 수정
    
    Feat #33

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -27,34 +27,34 @@ def code_crawling() :
 		        # 강의 계획서 클릭
 				find = driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[1]/div/div[5]/div[1]/div[4]/div[1]/div/div[%d]/div/div/div[23]/div/div'%index)
 				find.click()
-				time.sleep(1)
+				time.sleep(2)
 				# 과목코드 가져오기        
 				code = driver.find_element_by_id('ubiviewer_%d_CELL4191_1_14'%cc).text
 			except Exception: 
 				try :
 					# 과목코드 가져오기
 					code = driver.find_element_by_id('ubiviewer_%d_CELL983_1_14'%cc).text
-					time.sleep(1)
+					time.sleep(2)
 				except Exception:
 					try :
 						# 과목코드 가져오기
 						code = driver.find_element_by_id('ubiviewer_%d_CELL3949_1_10'%cc).text
-						time.sleep(1)
+						time.sleep(2)
 					except Exception:
 						try :
 							# 과목코드 가져오기
 							code = driver.find_element_by_id('ubiviewer_%d_CELL983_1_29'%cc).text
-							time.sleep(1)
+							time.sleep(2)
 						except Exception :
 							try :
 								# 과목코드 가져오기
 								code = driver.find_element_by_id('ubiviewer_%d_CELL983_1_31'%cc).text
-								time.sleep(1)
+								time.sleep(1.5)
 							except Exception as error : #강의계획서 없거나 찾지못한 예외 경우
 								print(error)
 								code = "NO"
 								cc -= 1
-								time.sleep(1)
+								time.sleep(1.5)
 			try :
 				# 강의 계획서 닫기
 				driver.find_element_by_id('mainframe_childframe_uniLessSubjtTblInqP01_titlebar_closebutton').click()
